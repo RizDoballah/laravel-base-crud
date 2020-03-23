73ab2f94b89b8a8b3f52efcddf1d1894279d5d61
Ghezlene Doballah <ghyzounette@gmail.com>
2020-03-23 16:42:55 +0100

    update

---
+++ database/migrations/2020_03_23_145012_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->text('joinDate');
+            $table->string('coach');
+            $table->string('team');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
